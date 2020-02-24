81b40ecdd1d5b56292f943150f3224733d6eb315
michele Reymond <hello@michelereymond.ch>
2020-02-24 16:14:49 +0100

    action create

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -3,6 +3,14 @@ class AlertsController < ApplicationController
   end
 
   def create
+    @alert = Alert.new()
+    @alert.user = current_user
+    @alert.started_at = DateTime.now
+    if @alert.save
+      redirect_to stop_path
+    else
+      render :new
+    end
   end
 
   def update
