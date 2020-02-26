211266d70171e8caadc647ade720654e561ff8ef
michele Reymond <hello@michelereymond.ch>
2020-02-26 18:00:54 +0100

    fix bug js

--- app/javascript/components/phonenumber.js
+++ app/javascript/components/phonenumber.js
@@ -1,9 +1,9 @@
 const addIndicatif = () => {
   const phoneNumberForm = document.querySelector(".phone-number > .form-group.string.required.contact_phone_number > .form-control")
-  const parent = phoneNumberForm.parentNode;
-  const wrapper = document.createElement('div');
 
   if(phoneNumberForm) {
+    const parent = phoneNumberForm.parentNode;
+    const wrapper = document.createElement('div');
     // set the wrapper as child (instead of the element)
     parent.replaceChild(wrapper, phoneNumberForm);
     // set element as child of wrapper
