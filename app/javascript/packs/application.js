cde9077716f30ca816cea85caf028e7c473ea45f
Nicoleta Dudau <dudaunicoleta@yahoo.com>
2020-02-27 10:15:44 +0100

    map with pin on today's alerts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,11 @@ import "bootstrap";
 import { initSendPosition } from "../plugins/geoloc.js";
 import { openNav } from '../components/sidebaropen';
 import { closeNav } from '../components/sidebarclose';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
 
 initSendPosition();
 openNav();
