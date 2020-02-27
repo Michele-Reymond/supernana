20901e81f612ded7ec777e39640d2ff9cb6c3dac
michele Reymond <hello@michelereymond.ch>
2020-02-27 14:58:09 +0100

    galère bodymovin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,12 @@ import { closeNav } from '../components/sidebarclose';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { addIndicatif } from '../components/phonenumber';
+import { animation } from '../scripts/index';
 
 
 openNav();
 closeNav();
+animation();
 initMapbox();
 initSendPosition();
 addIndicatif();
