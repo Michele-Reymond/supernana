a14f72d91b94b13c669f92773597c46232281763
michele Reymond <hello@michelereymond.ch>
2020-02-28 16:57:37 +0100

    test animation

--- app/javascript/components/bodymovin.js
+++ app/javascript/components/bodymovin.js
@@ -4,22 +4,14 @@ import animationData from '../../../public/images/data.json';
 
 const animation = () => {
 
-const loader = document.getElementById('bm')
-
     const animationTest = lottie.loadAnimation({
-    container: loader, // the dom element that will contain the animation
+    container: document.getElementById('bm'), // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
     animationData: animationData // the path to the animation json
   });
 
-  loader.addEventListener("mouseenter", function() {
-     animationTest.play();
-   });
-   loader.addEventListener("mouseleave", function() {
-     animationTest.stop();
-   });
   }
 
 export { animation };
