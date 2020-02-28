c7d4ab675d89c2957fac4c73697fa3281d855e8e
Nicoleta Dudau <dudaunicoleta@yahoo.com>
2020-02-28 10:22:27 +0100

    nav bar seulement avec infos pratiques

--- db/schema.rb
+++ db/schema.rb
@@ -48,13 +48,6 @@ ActiveRecord::Schema.define(version: 2020_02_27_131811) do
     t.index ["user_id"], name: "index_alerts_on_user_id"
   end
 
-  create_table "chats", force: :cascade do |t|
-    t.text "message"
-    t.string "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
