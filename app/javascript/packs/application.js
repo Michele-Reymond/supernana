0b6d7f95761c2c55a2ade027094e32438a9f00aa
michele Reymond <hello@michelereymond.ch>
2020-03-02 14:38:39 +0100

    bouton stopper l'alarme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,12 @@ import { closeNav } from '../components/sidebarclose';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { addIndicatif } from '../components/phonenumber';
+import { animationStop } from '../components/bmstopbutton';
 
 
 openNav();
 closeNav();
+animationStop();
 initMapbox();
 initSendPosition();
 addIndicatif();
-
